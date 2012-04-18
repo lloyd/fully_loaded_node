9a629a1b1ab80e334e06aa2f22fcff1927390bb8
Lloyd Hilaiel <lloyd@hilaiel.com>
2012-04-18 16:37:19 -0600

    revert cluster upgrade after merging pretty graph

--- bcrypt-compute.js
+++
@@ -1,7 +0,0 @@
-const bcrypt = require('bcrypt');
-
-process.on('message', function(m) {
-  var salt = bcrypt.genSaltSync(12);  
-  var hash = bcrypt.hashSync("B4c0/\/", salt);
-  process.send({});
-});
