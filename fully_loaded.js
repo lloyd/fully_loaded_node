35548f79a04b3dfa7029f79a1211741fa08b55a1
Lloyd Hilaiel <lloyd@hilaiel.com>
2012-04-23 14:15:33 -0400

    slides

---
+++ fully_loaded.js
@@ -0,0 +1,24 @@
+$(document).ready(function() {
+  function incHash() {
+    var nth = parseInt(window.location.hash.substr(1) || "1", 10) + 1;
+    nth = (nth > 3) ? 1 : nth;
+    window.location.hash = "#" + nth;
+  }
+
+  function showSlide(last) {
+    function showNew() {
+      var nth = window.location.hash.substr(1) || "1";
+      $("div.slide:nth-child(" + nth + ")").fadeIn(700);
+    }
+    if (last) $("div.slide:nth-child(" + last + ")").fadeOut(500, showNew);
+    else showNew()
+  }
+
+  showSlide(null);
+
+  $('body').click(function() {
+    var nth = window.location.hash.substr(1);
+    incHash();
+    showSlide(nth);
+  });
+});
